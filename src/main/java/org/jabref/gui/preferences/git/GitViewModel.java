12d25bc0645c15b372922f52786063634c220dfd
felicitasmaia <felicitas.haag@unibas.ch>
2024-11-05 14:22:52 +0100

    add classes for gui-preferences git tab

---
+++ src/main/java/org/jabref/gui/preferences/git/GitViewModel.java
@@ -0,0 +1,71 @@
+package org.jabref.gui.preferences.git;
+
+import java.util.List;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+import org.jabref.gui.preferences.PreferenceTabViewModel;
+import org.jabref.logic.git.AuthenticationViewMode;
+import org.jabref.logic.git.GitPreferences;
+
+public class GitViewModel implements PreferenceTabViewModel {
+
+    private final BooleanProperty gitSupportEnabledProperty = new SimpleBooleanProperty();
+    private final ObjectProperty<AuthenticationViewMode> authenticationMethod = new SimpleObjectProperty<>();
+
+    private final GitPreferences gitPreferences;
+
+    public GitViewModel(GitPreferences gitPreferences) {
+        this.gitPreferences = gitPreferences;
+    }
+
+    @Override
+    public void setValues() {
+        switch (gitPreferences.getAuthenticationMethod()) {
+            case SSH ->
+                    authenticationMethod.setValue(AuthenticationViewMode.SSH);
+            case CREDENTIALS ->
+                    authenticationMethod.setValue(AuthenticationViewMode.CREDENTIALS);
+        }
+        gitSupportEnabledProperty.setValue(gitPreferences.isGitEnabled());
+    }
+
+    /**
+     * Saves the current user preferences to the GroupsPreferences object.
+     */
+    @Override
+    public void storeSettings() {
+        gitPreferences.setGitSupportEnabledProperty(gitSupportEnabledProperty.getValue());
+
+        if (AuthenticationViewMode.SSH.equals(authenticationMethod.getValue())) {
+            gitPreferences.setAuthenticationMethod(AuthenticationViewMode.SSH);
+        } else if (AuthenticationViewMode.CREDENTIALS.equals(authenticationMethod.getValue())) {
+            gitPreferences.setAuthenticationMethod(AuthenticationViewMode.CREDENTIALS);
+        }
+    }
+
+    public BooleanProperty gitSupportEnabledProperty() {
+        return gitSupportEnabledProperty;
+    }
+
+    public AuthenticationViewMode authenticationViewMode() {
+        return authenticationMethod.getValue();
+    }
+
+    public ObjectProperty<AuthenticationViewMode> authenticationViewModeObjectProperty() {
+        return authenticationMethod;
+    }
+
+    @Override
+    public boolean validateSettings() {
+        return PreferenceTabViewModel.super.validateSettings();
+    }
+
+    @Override
+    public List<String> getRestartWarnings() {
+        return PreferenceTabViewModel.super.getRestartWarnings();
+    }
+}
